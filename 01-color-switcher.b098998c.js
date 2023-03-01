a3083f0b929f9116476ff9e38efac444e530a510
AMIHbKA <95082331+AMIHbKA@users.noreply.github.com>
2023-03-01 14:57:00 +0000

    Deploying to gh-pages from @ AMIHbKA/goit-js-hw-09@722551591bc3ba7cab127099ee6d33e919155901 🚀

---
+++ 01-color-switcher.b098998c.js
@@ -0,0 +1,2 @@
+!function(){document.querySelector("button[data-start]");var t=document.querySelector("button[data-stop]");t.disabled=!0,t.addEventListener("click")}();
+//# sourceMappingURL=01-color-switcher.b098998c.js.map
---
+++ 01-color-switcher.d69e0b52.js
@@ -0,0 +1,2 @@
+document.querySelector("button[data-start]");const t=document.querySelector("button[data-stop]");t.disabled=!0,t.addEventListener("click");
+//# sourceMappingURL=01-color-switcher.d69e0b52.js.map
