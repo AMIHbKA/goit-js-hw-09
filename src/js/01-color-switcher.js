7cfdff689bfe6a1e3136a96f8dcd00867fa92775
AMIHbKA <bishop0013forum@gmail.com>
2023-03-02 05:44:18 +0200

    tsk#1 finish

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,6 +1,28 @@
 const btnStart = document.querySelector('button[data-start]');
 const btnStop = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+let colorInterval = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onStopHadler() {
+  btnStop.disabled = true;
+  btnStart.disabled = false;
+  clearInterval(colorInterval);
+}
+
+function onStartHadler() {
+  btnStart.disabled = true;
+  btnStop.disabled = false;
+
+  colorInterval = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
 
 btnStop.disabled = true;
 
-btnStop.addEventListener('click');
+btnStop.addEventListener('click', onStopHadler);
+btnStart.addEventListener('click', onStartHadler);
