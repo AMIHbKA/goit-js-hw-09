3d263bbb0c2102edfdc8cfe38fdbeb2b000257ed
AMIHbKA <95082331+AMIHbKA@users.noreply.github.com>
2023-03-02 03:44:46 +0000

    Deploying to gh-pages from @ AMIHbKA/goit-js-hw-09@7cfdff689bfe6a1e3136a96f8dcd00867fa92775 🚀

--- 01-color-switcher.d69e0b52.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("button[data-start]");const t=document.querySelector("button[data-stop]");t.disabled=!0,t.addEventListener("click");
-//# sourceMappingURL=01-color-switcher.d69e0b52.js.map
