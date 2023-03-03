b4c8ca83efdad34a192da5eb845499c9732079bd
Олександр Шутяк <bishop0013forum@gmail.com>
2023-03-03 17:01:50 +0200

    tsk#3 begin

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,31 @@
+import { Notify } from 'notiflix';
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        // Fulfill
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      } else {
+        // Reject
+        reject(`❌ Rejected promise ${position} in ${delay}ms`);
+      }
+    }, delay);
+  });
+
+  return promise;
 }
+
+const refs = {
+  firstDelay: document.querySelector('input[name="delay"]'),
+  stepDelay: document.querySelector('input[name="step"]'),
+  amount: document.querySelector('input[name="amount"]'),
+};
+
+createPromise(1, 3000)
+  .then(f => Notify.success(f))
+  .catch(r => Notify.failure(r));
+createPromise(2, 1000)
+  .then(f => Notify.success(f))
+  .catch(r => Notify.failure(r));
