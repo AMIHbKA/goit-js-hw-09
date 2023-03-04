a13df5c063078165dd81609a57bb3bd6825fe2f7
AMIHbKA <bishop0013forum@gmail.com>
2023-03-04 06:04:08 +0200

    some code

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,38 @@
+import { Notify } from 'notiflix';
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        // Fulfill
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      } else {
+        // Reject
+        reject(`❌ Rejected promise ${position} in ${delay}ms`);
+      }
+    }, delay);
+  });
 }
+
+const refs = {
+  firstDelay: document.querySelector('input[name="delay"]'),
+  stepDelay: document.querySelector('input[name="step"]'),
+  amount: document.querySelector('input[name="amount"]'),
+  btnCreate: document.querySelector('button[type="submit"]'),
+};
+
+refs.btnCreate.addEventListener('click', () => {
+  console.log('click');
+});
+
+createPromise(1, 3000)
+  .then(f => Notify.success(f))
+  .catch(r => Notify.failure(r));
+createPromise(2, 1000)
+  .then(f => Notify.success(f))
+  .catch(r => Notify.failure(r));
+
+function onClickBtnCreatePromises() {
+    if ()
+  }
